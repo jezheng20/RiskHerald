8f689d93ff01b182f30be264bb1771bc5c80a12f
Tommy <tommyhe6@gmail.com>
2022-03-05 20:34:18 -0500

    backend working

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,10 +1,42 @@
-const index = ({ }) => {
-    // const intl = useIntl();
+import { useRouter } from 'next/router';
+import { Button } from "@chakra-ui/react";
+
+const index = ({ data }) => {
+    const router = useRouter();
+
+    const refreshData = () => {
+        router.replace(router.asPath);
+    }
+
     return (
         <div>
-            Home
+            {data.stuff}
+            <Button onClick={refreshData}>
+                REFRESH
+            </Button>
         </div>
     );
 };
 
 export default index;
+
+const fetchData = async () => {
+    const API = "http://127.0.0.1:5000/";
+    const res = await fetch(API, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+    const data = await res.json();
+    return data;
+};
+
+export const getServerSideProps = async () => {
+    const data = await fetchData();
+    return {
+        props: {
+            data
+        }
+    }
+};
