26027e0a232c583114f6fd58242055db6f992c1e
Tommy <tommyhe6@gmail.com>
2022-03-06 11:56:00 -0500

    threat ass

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,24 @@
 from flask import Flask
 from flask import jsonify
 import json
-# from threat_assessment_ondemand import threatAssessmentPipeline
+from threat_assessment_ondemand import threatAssessmentPipeline
+from flask import request
+from flask_cors import CORS, cross_origin
+import spacy
+import nltk
+from nltk.corpus import stopwords
 
 app = Flask(__name__)
+CORS(app)
+
+NER_MODEL = spacy.load('en_core_web_sm', 
+                       disable=["tok2vec", "tagger", "parser", "attribute_ruler"])
+EMB_MODEL = spacy.load('en_core_web_md')
+KEYWORD_ANCHORS = ['war', 'violence', 'attack', 'death', 'casualty', 'danger']
+STOPWORDS = stopwords.words('english')
+THREAT_THRESHOLDS = (2.0, 4.0)
+
+nltk.download('omw-1.4')
 
 @app.route("/", methods=["GET"])
 def put():
@@ -22,6 +37,21 @@ def put_static():
         data = json.load(f)
     return {
         "data": data
+    }
+
+
+@app.route("/threat_ass_demand", methods=["GET", "POST"])
+def threat_ass():
+    print("ASFSA")
+    a = request.get_json()
+    print(a)
+    return {
+        "data": threatAssessmentPipeline(url=a["input"],
+                                      ner_model=NER_MODEL,
+                                      emb_model=EMB_MODEL,
+                                      keyword_anchors=KEYWORD_ANCHORS,
+                                      stopwords=STOPWORDS,
+                                      threat_thresholds=THREAT_THRESHOLDS)
     }
 
 # @app.route("/demand", methods=["GET"])
--- frontend/pages/scan_risk.jsx
+++ frontend/pages/scan_risk.jsx
@@ -1,25 +1,49 @@
 import { fetchData, getPropsServerSide } from "../fetchUtils.js";
+import { useState } from "react";
+import { Button, Input } from "@chakra-ui/react";
 
 const evaluate = ({ }) => {
+    const [input, setInput] = useState("");
+    const [data, setData] = useState("");
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const onClick = async () => {
+        console.log("HERE")
+        const res = await fetch('http://127.0.0.1:5000/threat_ass_demand', {
+            headers: {
+                'Content-type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({ "input": input })
+        });
+        let json = await res.json();
+        let dt = json.data;
+        console.log(dt)
+        setData("Possible attack at " + dt['Possible location'] + " at " + dt['Possible times'][0] + " with " + dt['Threat message'])
+    }
+
     return (
         <>
-        <div className = "indexBack"></div>
-        <div className = "indexMain" style={{
-        	textAlign: 'center'
-        }}>
-            <div style = {{
+            <div className = "indexBack"></div>
+            <div className = "indexMain" style={{
+                textAlign: 'center'
+            }}>
+                <div style = {{
                     textAlign: "center",
                     marginBottom: "10px",
                     paddingLeft: "30%",
                     width: '70%'
                 }}>
-                <h1>Evaluate article</h1>
-                <button id='searchButton'>Run</button><input id='searchBar' type='text' placeholder='Enter URL...'></input>
+                    <h1>Evaluate article</h1>
+                    <Button onClick={onClick} id='searchButton'>Run</Button><Input onChange={handleChange} id='searchBar' type='text' placeholder='Enter URL...'></Input>
+                </div>
+                <div id="codeConsole">
+                    {data}
+                </div>
             </div>
-            <div id="codeConsole">
-            aaa
-            </div>
-        </div>
         </>
     );
 };
