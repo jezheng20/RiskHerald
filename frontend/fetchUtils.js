c51b78f12a5354bc6e8dafc1968511f2b485a55a
Tommy <tommyhe6@gmail.com>
2022-03-06 10:26:05 -0500

    add

--- frontend/fetchUtils.js
+++ frontend/fetchUtils.js
@@ -1,7 +1,7 @@
 export const fetchData = async () => {
     const API = "http://127.0.0.1:5000/";
     const res = await fetch(API, {
-        method: "PUT",
+        method: "GET",
         headers: {
             "Content-Type": "application/json"
         },
