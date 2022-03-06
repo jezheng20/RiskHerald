5a6e77ffd643c568113b830c0fbf41d200ce2a51
jzheng327 <jzheng327@gatech.edu>
2022-03-05 20:13:18 -0500

    Add Usable UI
    Sidebar works

---
+++ frontend/components/Aside.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useIntl } from 'react-intl';
+import {
+    ProSidebar,
+    Menu,
+    MenuItem,
+    SubMenu,
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
+} from 'react-pro-sidebar';
+import { FaUser, FaHome, FaLocationArrow, FaSistrix } from 'react-icons/fa';
+import sidebarBg from './assets/bg1.jpg';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { HStack, Box, useColorModeValue } from "@chakra-ui/react";
+import NextLink from 'next/link';
+
+
+const Aside = () => {
+    return (
+        <div style={{
+            height: '100%',
+            width: '200px',
+            position: 'fixed',
+            zIndex:1,
+            top: '0%',
+            left: '0',
+            backgroundColor: '#111',
+            overflowX: 'hidden'
+        }}>
+        <ProSidebar>
+            <SidebarHeader>
+	            <div
+	                style={{
+	                padding: '18px',
+	                fontWeight: 'bold',
+	                fontSize: 20,
+	                letterSpacing: '1px',
+	                overflow: 'hidden',
+	                textOverflow: 'ellipsis',
+	                whiteSpace: 'nowrap',
+	                }}>
+	            Risk Evaluation
+	            </div>
+            </SidebarHeader>
+            <Menu iconShape="square">
+                <NextLink href={"/"} passHref>
+                    <MenuItem icon={<FaHome />}>Home</MenuItem>
+                </NextLink>
+                <NextLink href={"/map"} passHref>
+                    <MenuItem icon={<FaLocationArrow />}>Map</MenuItem>
+                </NextLink>
+                <NextLink href={"/"} passHref>
+                    <MenuItem icon={<FaSistrix />}>Scan Risk</MenuItem>
+                </NextLink>
+                <NextLink href={"/"} passHref>
+                    <MenuItem icon={<FaUser />}>Near Me</MenuItem>
+                </NextLink>
+            </Menu>
+        </ProSidebar>
+        </div>
+
+    );
+};
+
+export default Aside;
+
--- frontend/components/Map.jsx
+++ frontend/components/Map.jsx
@@ -3,7 +3,7 @@ import 'leaflet/dist/leaflet.css'
 
 const Map = () => {
   return (
-    <MapContainer center={[49, 32]} zoom={6} scrollWheelZoom={true} style={{height: "100vh", width: "100%"}}>
+    <MapContainer center={[49, 32]} zoom={6} scrollWheelZoom={true} style={{height: "100vh", width: "100%",zIndex=0}}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
