904781bc8220f9319f7dbd4231d2f28b62a7bd86
jzheng327 <jzheng327@gatech.edu>
2022-03-05 20:53:22 -0500

    Revert "backend working"
    This reverts commit 8f689d93ff01b182f30be264bb1771bc5c80a12f.

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,42 +1,10 @@
-import { useRouter } from 'next/router';
-import { Button } from "@chakra-ui/react";
-
-const index = ({ data }) => {
-    const router = useRouter();
-
-    const refreshData = () => {
-        router.replace(router.asPath);
-    }
-
+const index = ({ }) => {
+    // const intl = useIntl();
     return (
         <div>
-            {data.stuff}
-            <Button onClick={refreshData}>
-                REFRESH
-            </Button>
+            Home
         </div>
     );
 };
 
 export default index;
-
-const fetchData = async () => {
-    const API = "http://127.0.0.1:5000/";
-    const res = await fetch(API, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-    });
-    const data = await res.json();
-    return data;
-};
-
-export const getServerSideProps = async () => {
-    const data = await fetchData();
-    return {
-        props: {
-            data
-        }
-    }
-};
